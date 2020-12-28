99ec227d820672325530c19944b2b907a8e48460
craig <craig.edney@hotmail.com>
2020-12-28 15:46:13 +0000

    aesthetic tweaks

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `LifelongPhD`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `LifelongPhD`,
+      summary: ``,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `The Lifelong PhD is designed to help you thrive in your personal and professional life.`,
+    siteUrl: `http://www.lifelongphd.com`,
     social: {
-      twitter: `kylemathews`,
+      twitter: ``,
     },
   },
   plugins: [
